f247277f3d9150a1fdd7f699d44f3736971048fc
Yannick Reekmans <yannick.reekmans@infront.be>
2019-01-10 20:02:13 +0100

    Turn back the config file changes

--- GhostReleaseFunction/AzureDeployment/deploy.cmd
+++ GhostReleaseFunction/AzureDeployment/deploy.cmd
@@ -106,19 +106,19 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   popd
 )
 
-if NOT EXIST "%DEPLOYMENT_TARGET%\config.development.json" (
-	echo Init config.development.json
-	copy "%DEPLOYMENT_TARGET%\config.development.json.init" "%DEPLOYMENT_TARGET%\config.development.json"
-) else (
-	echo Skipping config.development.json
-)
+:: if NOT EXIST "%DEPLOYMENT_TARGET%\config.development.json" (
+:: 	echo Init config.development.json
+:: 	copy "%DEPLOYMENT_TARGET%\config.development.json.init" "%DEPLOYMENT_TARGET%\config.development.json"
+:: ) else (
+:: 	echo Skipping config.development.json
+:: )
 
-if NOT EXIST "%DEPLOYMENT_TARGET%\config.production.json" (
-	echo Init config.production.json
-	copy "%DEPLOYMENT_TARGET%\config.production.json.init" "%DEPLOYMENT_TARGET%\config.production.json"
-) else (
-	echo Skipping config.production.json
-)
+:: if NOT EXIST "%DEPLOYMENT_TARGET%\config.production.json" (
+:: 	echo Init config.production.json
+:: 	copy "%DEPLOYMENT_TARGET%\config.production.json.init" "%DEPLOYMENT_TARGET%\config.production.json"
+:: ) else (
+:: 	echo Skipping config.production.json
+:: )
 
 :: 4. Handle database creation and migrations.
 IF EXIST "%DEPLOYMENT_TARGET%\db.js" (
